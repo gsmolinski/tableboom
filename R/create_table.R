7be21a4cfc0adfb5583aa5f0128674a9bd564e46
gsmolinski <g.smolinski1@gmail.com>
2022-10-22 17:18:17 +0200

    in output: highlight string, quote, dots

--- R/create_table.R
+++ R/create_table.R
@@ -144,10 +144,14 @@ highlight_syntax <- function(code) {
 #' @noRd
 highlight_output <- function(output) {
   output |>
+    stringi::stri_replace_all_regex('["](.*?)["]', '<span class = "quote_output">"</span><span class = "string_output">$1</span><span class = "quote_output">"</span>') |>
+    stringi::stri_replace_all_regex("['](.*?)[']", "<span class = 'quote_output'>'</span><span class = 'string_output'>$1</span><span class = 'quote_output'>'</span>") |>
     stringi::stri_replace_all_regex("(\\.*[\\w.]+|`.+`)(?=\\()", "<span class = 'fun_call_output'>$1</span>") |>
     stringi::stri_replace_all_regex("(\\w+:{3}|\\w+:{2})", "<span class = 'namespace_output'>$1</span>") |>
     stringi::stri_replace_all_fixed("(", "<span class = 'bracket_output'>(</span>") |>
-    stringi::stri_replace_all_fixed(")", "<span class = 'bracket_output'>)</span>")
+    stringi::stri_replace_all_fixed(")", "<span class = 'bracket_output'>)</span>") |>
+    stringi::stri_replace_all_fixed("...", "<span class = 'dots_output'>...</span>") |>
+    stringi::stri_replace_all_fixed("…", "<span class = 'dots_output'>…</span>")
 }
 
 #' Remove Dots From The Beginning Of Output
